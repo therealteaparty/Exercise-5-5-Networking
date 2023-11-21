7b11af5a734cd736962748ff4c7eab11b768d0be
therealteaparty <142958423+therealteaparty@users.noreply.github.com>
2023-11-21 14:11:23 -0500

    Completes Exercise 5.5

--- Player/Player.gd
+++ Player/Player.gd
@@ -42,6 +42,21 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	rpc("_set_position", global_position)
+	rpc("_set_rotation", rotation.y)
 
 func die():
+	rpc("_die")
+	queue_free()
+	
+@rpc("any_peer","call_remote","unreliable_ordered")
+func _set_position(p):
+	global_position = p
+
+@rpc("any_peer","call_remote","unreliable_ordered")
+func _set_rotation(r):
+	rotation.y = r
+
+@rpc("any_peer","call_remote","unreliable_ordered")
+func _die():
 	queue_free()
